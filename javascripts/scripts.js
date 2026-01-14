c9567905a21ea015f591ec2a0cf44a52342dddc6
devDanielFelix <danielfelix72@hotmail.com>
2026-01-13 23:03:59 -0300

    scripts

---
+++ javascripts/scripts.js
@@ -0,0 +1,9 @@
+const Products = [
+    {
+        name: "Orange_Denver_Broncos",
+        price: 399.99,
+        image: "catalogo/Denver_broncos_jersey_Sutton.jpg",
+        category: "Jerseys",
+        team: "Denver Broncos"
+    },
+];
